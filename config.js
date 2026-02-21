00e2813cab0734643702afe0a2c4f9ddb09be8fe
napoli-bot-notizie <antoniocontea676733449@gmail.com>
2026-02-21 20:24:23 +0100

    Update config.js
    656666

--- config.js
+++ config.js
@@ -15,7 +15,7 @@ global.wait = 'ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ...';
 
 
 global.owner = [
-  ['393773842461', '𝐂𝐡𝐚𝐭𝐔𝐧𝐢𝐭𝐲', true],
+  ['393336413960', 'creatore, true],
   ['xxxxxxxxxx'], //mettete il vostro numero al posto delle x e copiate sopra il formato dopo ovvero 'nome', true
   ['xxxxxxxxxx'],
   ['xxxxxxxxxx'],
